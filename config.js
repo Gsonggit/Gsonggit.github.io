eb826f5edc6d949efb4e50b766b7ca41d7b64b08
gaosong <532590979@qq.con>
2020-09-28 23:15:30 +0800

    add10

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "3",
+        turn_on:"3",
         play:"2",
         bannar_coming: "1",
         balloons_flying: "开始",
